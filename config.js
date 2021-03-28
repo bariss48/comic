dac118244d98d0c6ce5c1aa8ca0bdc322ad8c38c
Barış <50153950+bariss48@users.noreply.github.com>
2021-03-28 21:15:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,7 @@ const config = {
 
      db:{
 
-       username: "admin",
-       password: "wFi3gs09W3Y8bhC7",
-       connection:"mongodb+srv://admin:wFi3gs09W3Y8bhC7@comics-project.8fsjn.mongodb.net/yelp_comics?retryWrites=true&w=majority"
-
-
+ 
 
      }
 }
